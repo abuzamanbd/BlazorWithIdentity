8854e437a4aa9ac3c688ca9acb67109acd09df4b
Abu Zaman <md.zaman1994@gmail.com>
2021-10-29 01:58:54 +0600

    1st commit

---
+++ BlazorIdentity/Areas/Identity/Data/BlazorIdentityContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorIdentity.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorIdentity.Data
+{
+    public class BlazorIdentityContext : IdentityDbContext<BlazorIdentityUser>
+    {
+        private string RoleIdAdmin = Guid.NewGuid().ToString();
+        private string RoleIdMember = Guid.NewGuid().ToString();
+        private string RoleIdUser = Guid.NewGuid().ToString();
+
+        public BlazorIdentityContext(DbContextOptions<BlazorIdentityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+                .HasData(new IdentityRole 
+                { 
+                    Id = RoleIdAdmin,
+                    Name = "admin", NormalizedName = "ADMIN",
+                    ConcurrencyStamp = DateTime.UtcNow.ToString()
+                },
+                new IdentityRole
+                {
+                    Id = RoleIdMember,
+                    Name = "member",
+                    NormalizedName = "MEMBER",
+                    ConcurrencyStamp = DateTime.UtcNow.ToString()
+                },
+                new IdentityRole
+                {
+                    Id = RoleIdUser,
+                    Name = "user",
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = DateTime.UtcNow.ToString()
+                }
+            );
+        }
+    }
+}
---
+++ BlazorIdentity/Startup.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using BlazorIdentity.Areas.Identity;
+using BlazorIdentity.Areas.Identity.Data;
+using BlazorIdentity.Data;
+
+namespace BlazorIdentity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<BlazorIdentityUser>>();
+            services.AddSingleton<WeatherForecastService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<BlazorIdentityUser> userManager)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+            ApplicationDbInitializer.SeedUsers(userManager);
+        }
+
+        //https://stackoverflow.com/questions/50785009/how-to-seed-an-admin-user-in-ef-core-2-1-0
+        public static class ApplicationDbInitializer
+        {
+            public static void SeedUsers(UserManager<BlazorIdentityUser> userManager)
+            {
+                if (userManager.FindByEmailAsync("admin@example.com").Result == null)
+                {
+                    BlazorIdentityUser user = new BlazorIdentityUser
+                    {
+                        UserName = "admin@example.com",
+                        Email = "admin@example.com",
+                        EmailConfirmed = true                        
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "P@ssword1234").Result;
+
+                    //if (result.Succeeded)
+                    //{
+                    //    userManager.AddToRoleAsync(user, "admin").Wait();
+                    //}
+                }
+            }
+        }
+    }
+}
